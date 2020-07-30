98dbd9de094363c55f44210005d6e47aa461dbc6
Victor Grandclement <vgrandclement@webqam.fr>
2020-07-30 09:53:19 +0200

    add layout component

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,6 +20,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('layout-component', require('./components/LayoutComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/LayoutComponent.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <div class="layout">
+        <header-component></header-component>
+
+        <div class="layout__content">
+            <explication-component></explication-component>
+        </div>
+        
+        <footer-component></footer-component>
+    </div>
+</template>
+
+<script>
+    import ExplicationComponent from './ExplicationComponent';
+    import HeaderComponent from './HeaderComponent';
+    import FooterComponent from './FooterComponent';
+
+    export default {
+        components: {
+            HeaderComponent,
+            FooterComponent,
+            ExplicationComponent
+            
+        },
+        props : {
+            title : {
+                type: String
+            },
+            description : {
+                type: String
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+    }
+</script>
