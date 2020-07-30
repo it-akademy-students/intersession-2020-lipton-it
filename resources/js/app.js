535e56c9520b80bfc9b0362b8b25d7ca5bae0da9
Victor Grandclement <vgrandclement@webqam.fr>
2020-07-30 09:55:53 +0200

    add header & footer

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,6 +20,8 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('header-component', require('./components/HeaderComponent.vue').default);
+Vue.component('footer-component', require('./components/FooterComponent.vue').default);
 Vue.component('layout-component', require('./components/LayoutComponent.vue').default);
 
 /**
