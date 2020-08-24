575eb18764074d9846db276a2db8f49d0e1063d9
Olivier Charon <olivier.charon@outlook.fr>
2020-08-24 15:11:38 +0200

    Cleanning

--- resources/js/components/DonationComponent.vue
+++ resources/js/components/DonationComponent.vue
@@ -1,11 +1,41 @@
 <template>
-    <div>
-        <h1  class="layout__title">Faites un don </h1>
-        <p   class="layout__description">Vous pouvez soutenir le projet et l’équipe de développeur en faisant un don via paypal  lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum solor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet</p>
-    </div>
+  <div>
+    <h1 class="layout__title">Faites un don</h1>
+    <p
+      class="layout__description"
+    >Vous pouvez soutenir le projet et l’équipe de développeur en faisant un don via paypal lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum solor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet</p>
+    <button @click="donate()" class="btn btn-danger">
+      "Donnez-moi"
+      <br />Enrico Macias, 2013
+    </button>
+  </div>
 </template>
 
 <script>
-    export default {
-    };
+export default {
+  methods: {
+    donate() {
+      console.log("a donné");
+    },
+  },
+};
 </script>
+
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+}
+
+div * {
+  margin: 25px 0;
+}
+
+button {
+  width: 75%;
+  align-self: center;
+  font-size: 1em;
+  padding: 10px 0;
+}
+</style>
--- resources/js/components/ExplicationComponent.vue
+++ resources/js/components/ExplicationComponent.vue
@@ -10,11 +10,6 @@
       Quelques minutes plus tard vous recevrez par mail un compte rendu détaillant les failles trouvés et une note sur 100, vous indiquant à quel point votre projet est sur.
       En attendant prenez un café.
     </p>
-
-    <button @click="donate()" class="btn btn-danger">
-      "Donnez-moi"
-      <br />Enrico Macias, 2013
-    </button>
   </div>
 </template>
 
@@ -28,20 +23,3 @@ export default {
 };
 </script>
 
-<style scoped>
-div {
-  display: flex;
-  flex-direction: column;
-}
-
-div * {
-  margin: 25px 0;
-}
-
-button {
-  width: 75%;
-  align-self: center;
-  font-size: 1em;
-  padding: 10px 0;
-}
-</style>
