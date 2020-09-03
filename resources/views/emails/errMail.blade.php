736eda4e185f6a68e7328be2f7508e1a51a803aa
admin <admin@iMac-ITA-V12.local>
2020-09-03 17:18:35 +0200

    fix error merge

---
+++ resources/views/emails/errMail.blade.php
@@ -0,0 +1,17 @@
+@component('mail::message')
+<style>
+    body > table.wrapper > tbody > tr > td > table.content > tbody > tr:nth-child(1) > td.header {
+        visibility: hidden;
+    }
+</style>
+
+# Houston, we have a problem...
+
+<p style="text-align:center;"><img src="{{ asset('images/lipton-it.png') }}" height="250"></p>
+
+Hey !
+
+Nous avons rencontré un probleme technique.<br>
+Merci de bien vouloir essayer de nouveau.<br>
+{{ config('app.name') }}
+@endcomponent
