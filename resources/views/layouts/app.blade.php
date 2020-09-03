202d02c10c14d3c88267af24565f31c1c39a158a
admin <admin@iMac-ITA-V12.local>
2020-09-03 15:25:59 +0200

    add favicon & clear images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="icon" href="{{ asset('images/icon.png') }}" />
 </head>
 <body>
     <div id="app">
