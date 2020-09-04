433869d09a072e5b202a66425c9976173164833b
admin <admin@iMac-ITA-V12.local>
2020-09-04 13:33:20 +0200

    clean code + edit message

--- resources/js/components/ExplicationComponent.vue
+++ resources/js/components/ExplicationComponent.vue
@@ -5,8 +5,8 @@
       Lipton IT est un projet Open Source.
       Très sensible à la sécurité, notre but est d’offrir un outil supplémentaire pour rendre votre code plus sur.
       Vous nous transmettez le lien du répertoir github de votre projet en PHP, notre programme passe alors en revue votre code.
-      Quelques minutes plus tard vous recevrez par mail un compte rendu détaillant les failles trouvés et une note sur 100, vous indiquant à quel point votre projet est sur.
-      En attendant prenez un café.
+      Quelques minutes plus tard vous recevrez par mail en pièce jointe un compte rendu détaillant les failles trouvés, vous indiquant à quel point votre projet est sur.
+      En attendant prenez un soda.
     </p>
   </div>
 </template>
