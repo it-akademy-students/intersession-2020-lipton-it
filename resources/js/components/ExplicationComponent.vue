787e7cbe1856625768bca92bad88471d4b31a5c7
methbkts <30674934+methbkts@users.noreply.github.com>
2020-09-04 13:36:48 +0200

    Update ExplicationComponent.vue

--- resources/js/components/ExplicationComponent.vue
+++ resources/js/components/ExplicationComponent.vue
@@ -3,10 +3,10 @@
     <h1 class="layout__title">Explication</h1>
     <p class="layout__description">
       Lipton IT est un projet Open Source.
-      Très sensible à la sécurité, notre but est d’offrir un outil supplémentaire pour rendre votre code plus sur.
-      Vous nous transmettez le lien du répertoir github de votre projet en PHP, notre programme passe alors en revue votre code.
-      Quelques minutes plus tard vous recevrez par mail en pièce jointe un compte rendu détaillant les failles trouvés, vous indiquant à quel point votre projet est sur.
-      En attendant prenez un soda.
+      Très sensible à la sécurité, notre but est d’offrir un outil supplémentaire pour rendre votre code plus sûr.
+      Vous nous transmettez le lien du répertoire Github de votre projet PHP, notre programme passe alors en revue votre code.
+      Quelques minutes plus tard vous recevrez par mail, en pièce jointe, un compte rendu détaillant les failles trouvées, vous indiquant à quel point votre projet est sûr.
+      En attendant servez vous un soda.
     </p>
   </div>
 </template>
